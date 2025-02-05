496f16a3dceb08426d5ec4e0e24b9c081e488b54
SumitGarg <garg10sumit@gmail.com>
2025-02-05 16:08:38 +0530

    commit

--- 133_clone_graph.cpp
+++ 133_clone_graph.cpp
@@ -1,32 +1,47 @@
+// Definition for a Node.
 #include <bits/stdc++.h>
 using namespace std;
-
-class Solution
-{
+class Node {
 public:
-    unordered_map<Node *, Node *> mpp;
-    void dfs(Node *node, Node *clone_node)
-    {
-        for (auto n : node->neighbors)
-        {
-            if (mpp.find(n) == mpp.end())
-            {
-                Node *clone = new Node(n->val);
-                mpp[n] = clone;
+    int val;
+    vector<Node*> neighbors;
+    
+    Node() {
+        val = 0;
+        neighbors = vector<Node*>();
+    }
+    
+    Node(int _val) {
+        val = _val;
+        neighbors = vector<Node*>();
+    }
+    
+    Node(int _val, vector<Node*> _neighbors) {
+        val = _val;
+        neighbors = _neighbors;
+    }
+};
+
+class Solution {
+public:
+    unordered_map<Node*, Node*> mpp; // To store mapping of original node to cloned node
+    
+    void dfs(Node* node, Node* clone_node) {
+        for (auto n : node->neighbors) {
+            if (mpp.find(n) == mpp.end()) { // If not already cloned
+                Node* clone = new Node(n->val);
+                mpp[n] = clone; // Store the cloned node
                 clone_node->neighbors.push_back(clone);
-                dfs(n, clone);
-            }
-            else
-            {
-                clone_node->neighbors.push_back(mpp[n]);
+                dfs(n, clone); // Recur for neighbors
+            } else {
+                clone_node->neighbors.push_back(mpp[n]); // Already cloned, just connect
             }
         }
     }
-    Node *cloneGraph(Node *node)
-    {
-        if (!node)
-            return NULL;
-        Node *clone_node = new Node(node->val);
+    
+    Node* cloneGraph(Node* node) {
+        if (!node) return NULL;
+        Node* clone_node = new Node(node->val);
         mpp[node] = clone_node;
         dfs(node, clone_node);
         return clone_node;
